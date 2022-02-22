7f4be176a5b82230338a3d646d34b529f35ba1e5
Elena <elena.nikolova190@gmail.com>
2022-02-22 21:46:01 +0000

    Moved models to models.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #----------------------------------------------------------------------------#
 # Imports
 #----------------------------------------------------------------------------#
+
 import sys
 import json
 import dateutil.parser
@@ -15,6 +16,7 @@ from sqlalchemy import Column, ForeignKey, Integer
 from forms import *
 from flask_migrate import Migrate
 from datetime import datetime
+from models import Venue, Artist, Artist_Genre, Venue_Genre, Show
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -25,92 +27,11 @@ moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-# from models import db, Venue, Artist
 migrate = Migrate(app, db)
-
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-
-class Venue(db.Model):
-    __tablename__ = 'venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120), nullable=False)
-    genres = db.relationship('Venue_Genre', backref='venue', lazy=True)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(300))
-    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
-    seeking_description = db.Column(db.String())
-    shows = db.relationship('Show',backref='venue',lazy=True,
-                        cascade="save-update, merge, delete")
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-
-class Artist(db.Model):
-    __tablename__ = 'artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120), nullable=False)
-    genres = db.relationship("Artist_Genre", backref="artist", lazy=True)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, nullable=True, default=False)
-    seeking_description = db.Column(db.String(), nullable=True, default="")
-    shows = db.relationship('Show',backref='artist',lazy=True,
-                        cascade="save-update, merge, delete")
-
-    def __repr__(self):
-        return f'<Artists ID:{self.id}, name: {self.name}, city: {self.city}>'
-
-
-class Artist_Genre(db.Model):
-    __tablename__ = 'artist_genres'
-    id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey(
-        'artist.id'), nullable=False)
-    genre = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return f'<Artist_Genre artist_id:{self.artist_id} genre: {self.genre}>'
-
-
-class Venue_Genre(db.Model):
-    __tablename__ = 'venue_genres'
-    id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey(
-        'venue.id'), nullable=False)
-    genre = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return f'<Venue_Genre venue_id:{self.venue_id} genre: {self.genre}>'
-
-
-class Show(db.Model):
-    __tablename__ = 'shows'
-    id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey(
-        'artist.id'), nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False)
-
-    def __repr__(self):
-        return f'<Shows show_id: {self.id} venue_id: {self.venue_id} artist_id: {self.artist_id} start_time: {self.start_time} >'
 
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
-
 
 def format_datetime(value, format='medium'):
     date = dateutil.parser.parse(value)
